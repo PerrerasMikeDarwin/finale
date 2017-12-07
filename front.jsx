ba1b8579083800f9fc3cece8d52dc33a9aed1231
Mike Darwin Sandoval Perreras <mikedarwin.perreras@gmail.com>
2017-12-07 14:16:19 +0800

    Add files via upload

---
+++ front.jsx
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+img {
+	opacity: 0.8;
+	filter: alpha(opacity=80);
+}
+</style>
+
+</head>
+<title> Index </title>
+<body bgcolor="yellow">
+<img src="psu.png" width="300" height="300" align="right" >
+ 
+<h1><b>Pangasinan<br>
+<br>State<br>
+<br>University</b></h1><br>
+
+
+<p><center><a href="history.html">Next</a></center></p>
+<p><center><a href="index.html">Home</a></center></p>
+</body>
+    
+</html>
