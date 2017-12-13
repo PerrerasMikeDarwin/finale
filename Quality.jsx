5a2d08eb9bc2dccb5efe92127b4b8bb46b900654
Mike Darwin Sandoval Perreras <mikedarwin.perreras@gmail.com>
2017-12-13 12:10:06 +0800

    Add files via upload

---
+++ Quality.jsx
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+img {
+	opacity: 0.8;
+	filter: alpha(opacity=80);
+}
+
+</style>
+</head>
+<title>QUALITY</title>
+<body bgcolor="yellow">
+<img src="psu.png" width="300" height="300" align="right">
+ 
+<h1>QUALITY POLICY</h1> <h2>
+The Pangasinan State University shall be<br>
+recognized as an ASEAN premier state<br>
+university that provides quality education and<br>
+satisfactory service delivery through<br>
+instruction, research, extension and<br>
+production.<br>
+<br>
+<br>
+We commit our expertise and resources to<br>
+produce professionals who meet the<br>
+expectations of the industry and other<br>
+interested parties in the national and<br>
+international community.<br>
+<br>
+<br>
+We shall continuously improve our operations<br>
+in response to the changing environment and<br>
+in support of the institution's strategic<br>
+direction.<br></h2>
+
+<br>
+<audio controls>
+  <source src="Quality.mp3" type="audio/mp3">
+Your browser does not support the audio element.
+</audio>
+<p><center><a href="song.jsx">Next</a><center></p>
+<p><center><a href="core.jsx">Back</a></center></p>
+<p><center><a href="index.html">Home</a></center></p>
+</body>
+</html>
